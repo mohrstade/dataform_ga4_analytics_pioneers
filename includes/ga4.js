11b99ca719abbf59005ad317736e8e4b0677192f
Moritz Bauer <moritz@mohrstade.de>
2023-02-27 13:26:42 +0000

    init

--- includes/ga4.js
+++
@@ -1,5 +0,0 @@
-const project_id = "bigquery-public-data";
-const dataset_id = "ga4_obfuscated_sample_ecommerce";
-const table_id = "events_*";
-
-module.exports = {project_id, dataset_id, table_id}
